53be1366f83f7079930b213ab7c555d7b602d153
Pasan Mudalige <pasanmu@insharptechnologies.com>
2025-08-02 01:47:01 +0530

    step 3

--- src/pages/auth/WebPage.vue
+++ src/pages/auth/WebPage.vue
@@ -1,16 +1,21 @@
 <template>
   <q-page class="flex flex-center bg-grey-1">
     <div class="column items-center">
+      <!-- Logo on top -->
+      <img src="~assets/galena.png" alt="Logo" class="q-mb-lg" style="width: 300px; height: auto" />
+
       <div class="text-center q-pa-md">
         <h1 class="text-primary text-h2 font-weight-bold q-mb-md">Coming Soon</h1>
-        <p class="text-h6 q-mb-lg text-grey-8">We're working hard to bring you something amazing!</p>
-            <q-btn
-              no-caps
-              unelevated
-              color="primary"
-              label="Click here to log in"
-              to="/admin"
-              class="q-mt-md"
+        <p class="text-h6 q-mb-lg text-grey-8">
+          We're working hard to bring you something amazing!
+        </p>
+        <q-btn
+          no-caps
+          unelevated
+          color="primary"
+          label="Click here to log in"
+          to="/admin"
+          class="q-mt-md"
         />
       </div>
     </div>
@@ -18,8 +23,7 @@
 </template>
 
 <script setup>
-
 defineOptions({
-  name: 'ComingSoonPage'
+  name: 'ComingSoonPage',
 })
 </script>
